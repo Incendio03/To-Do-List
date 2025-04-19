73e2ce49510ee42d3c0b2344796aa1d600915f95
GEMUEL CORNILLEZ <gemueldajaycornillez152003@gmail.com>
2025-04-19 23:50:36 +0800

    Established MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
@@ -13,16 +14,20 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// Basic route for testing
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the ToDo List API' });
-});
-
 // Routes
 // app.use('/api/auth', authRoutes);
 // app.use('/api/tasks', taskRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+.then(() => {
+  console.log("Connected to database!");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch(() => {
+  console.log("Failed connecting to database!");
+})
