d71af0d1836fbc032b0768b1f02e0828d4852161
Gemuel Cornillez <gemueldajaycornillez152003@gmail.com>
2025-04-20 22:13:10 +0800

    Implemented Routing on Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginPage } from './components/authentication/Login';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage>
-      </LoginPage>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage/>} />
+          <Route path="/login" element={<LoginPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
