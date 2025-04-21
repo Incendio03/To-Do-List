b9af6e945baba83ac82b78ad57809db19f607c26
Gemuel Cornillez <gemueldajaycornillez152003@gmail.com>
2025-04-21 22:18:11 +0800

    Added UI design for login page
    - Forgot Password is not working yet
    - To be added soon: Login with FB or Google

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { loginUser } from "../../services/authService";
+import '../../styles/authentication/login.css';
 
 export function LoginPage() {
     const [username, setUsername] = useState('');
@@ -15,20 +16,36 @@ export function LoginPage() {
     }
 
     return (
-        <div>
-            <input 
-                placeholder="Enter Username"
-                value = {username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
+        <div className="loginPage">
+            <section className="introContainer">
+                <h1>Lexo</h1>
+                <p>Your modern workspace for managing tasks with clarity and control. <br/>Stay focused, organized, and ahead of your day — all in one to-do list app.</p>
+            </section>
+            <div className="loginContainer">
+                <div className="inputContainer">
+                    <input
+                        className="loginInputs" 
+                        placeholder="Enter Username"    
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
 
-            <input 
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
+                    <input
+                        className="loginInputs"  
+                        placeholder="Enter Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
 
-            <button onClick={onLogin}>Login</button>
+
+                </div>
+
+                <a href="#" className="forgotPassword">Forgot Password?</a>
+
+                <button className="loginBtn" onClick={onLogin}>Login</button>
+
+                <p className="register">Not yet registered? <a href="/signup">Register here</a></p>
+            </div>
         </div>
     );
 }
